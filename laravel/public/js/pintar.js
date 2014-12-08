fdccc763873e29885a2a7f064887ba29cb27af68
Charlie400 <entrenarydefinir@gmail.com>
2014-12-08 16:07:45 +0100

    Revert "Revert "Dibujar en la Pizarra""
    This reverts commit c750e4fe255cb8c30f97c35dd80051963cab2fa6.

---
+++ laravel/public/js/pintar.js
@@ -0,0 +1,70 @@
+var pizarra_canvas;
+var pizarra_context;
+ 
+
+pizarra_canvas = document.getElementById("c");
+pizarra_context = pizarra_canvas.getContext("2d");
+pizarra_context.strokeStyle = "#000";
+pizarra_context.lineWidth = document.grosor.valorgrosor.selectedIndex+1;
+pizarra_canvas.addEventListener("mousedown",empezarPintar,false);
+pizarra_canvas.addEventListener("mouseup",terminarPintar,false);
+
+$('.ColorPicker').on('click', cambiarColor);
+
+function rgb2hex(rgb) {
+    rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+    function hex(x) {
+        return ("0" + parseInt(x).toString(16)).slice(-2);
+    }
+    return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+}
+
+function cambiarColor(){
+	pizarra_context.strokeStyle = rgb2hex($(this).css('background-color'));
+}
+ 
+function cambiaTrazado(){
+	pizarra_context.lineWidth = document.grosor.valorgrosor.selectedIndex+1;
+}
+
+/*
+	empezarPintar(e)
+	Al hacer mousedown sobre la pizarra, comenzamos un nuevo trazo, movemos el pincel hasta la 
+	posición del ratón y añadimos un listener para el evento mousemove, para que con cada movimiento 
+	del ratón se haga un nuevo trazo
+*/
+ 
+ 
+function empezarPintar(e){
+	pizarra_context.beginPath();
+	pizarra_context.moveTo(e.clientX-pizarra_canvas.offsetLeft,e.clientY-pizarra_canvas.offsetTop);
+	pizarra_canvas.addEventListener("mousemove",pintar,false)
+}
+ 
+/*
+	terminarPintar(e) se ejecuta al soltar el botón izquierdo, y elimina el listener para 
+	mousemove
+*/
+ 
+function terminarPintar(e){
+	pizarra_canvas.removeEventListener("mousemove",pintar,false);
+}
+ 
+/*
+	pintar(e) se ejecuta cada vez que movemos el ratón con el botón izquierdo pulsado.
+	Con cada movimiento dibujamos una nueva linea hasta la posición actual del ratón en pantalla.
+*/
+ 
+function pintar(e) {
+	pizarra_context.lineTo(e.clientX-pizarra_canvas.offsetLeft,e.clientY-pizarra_canvas.offsetTop);
+	pizarra_context.stroke();
+}
+ 
+/*
+	borrar() vuelve a setear el ancho del canvas, lo que produce que se borren los trazos dibujados
+	hasta ese momento.
+*/
+ 
+function borrar(){
+	pizarra_canvas.width = pizarra_canvas.width;
+}
