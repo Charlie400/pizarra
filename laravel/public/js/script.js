c750e4fe255cb8c30f97c35dd80051963cab2fa6
Charlie400 <entrenarydefinir@gmail.com>
2014-12-08 16:00:18 +0100

    Revert "Dibujar en la Pizarra"
    This reverts commit c51016a7a1977a87df4c8e3004011b9b2f0e7b0b.

--- laravel/public/js/script.js
+++ laravel/public/js/script.js
@@ -20,9 +20,6 @@ Name = {
 },
 serverURL = location.protocol+'//'+location.hostname+'/laravel/public';
 
-$('#c').attr('width', $('.Pizarra').width());
-$('#c').attr('height', $('.Pizarra').height());
-
 $('.MenuLeft-ItemLista-Esc').on('click', ListaClick1);
 function ListaClick1(){
 
@@ -367,56 +364,3 @@ function getDominios()
 }
 
 /*------------TERMINAN LAS FUNCIONES QUE USAN AJAX PARA INTERACTUAR CON EL CONTENIDO-----------*/
-
-
-/*------------ FUNCIÓN DRAG AND DROP-----------------------------------------------------------*/
-(function($) {
-    $.fn.drags = function(opt) {
-
-        opt = $.extend({handle:"",cursor:"move"}, opt);
-
-        if(opt.handle === "") {
-            var $el = this;
-        } else {
-            var $el = this.find(opt.handle);
-        }
-
-        return $el.css('cursor', opt.cursor).on("mousedown", function(e) {
-            if(opt.handle === "") {
-                var $drag = $(this).addClass('draggable');
-            } else {
-                var $drag = $(this).addClass('active-handle').parent().addClass('draggable');
-            }
-            var z_idx = $drag.css('z-index'),
-                drg_h = $drag.outerHeight(),
-                drg_w = $drag.outerWidth(),
-                pos_y = $drag.offset().top + drg_h - e.pageY,
-                pos_x = $drag.offset().left + drg_w - e.pageX;
-            $drag.css('z-index', 1000).parents().on("mousemove", function(e) {
-                $('.draggable').offset({
-                    top:e.pageY + pos_y - drg_h,
-                    left:e.pageX + pos_x - drg_w
-                }).on("mouseup", function() {
-                    $(this).removeClass('draggable').css('z-index', z_idx);
-                });
-            });
-            e.preventDefault();
-        }).on("mouseup", function() {
-            if(opt.handle === "") {
-                $(this).removeClass('draggable');
-            } else {
-                $(this).removeClass('active-handle').parent().removeClass('draggable');
-            }
-        });
-
-    }
-})(jQuery);
-
-/*-------------------------- TERMINA FUNCIÓN DRAG AND DROP ---------------------------------------*/
-
-function dibujoManoAlzada(){
-	$(".ColorPickerContainer").toggle();
-}
-
-$('.ColorPickerContainer').drags();
-
